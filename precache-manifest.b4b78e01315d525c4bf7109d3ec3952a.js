bb9a26562c2e103158b689897a60ad08697d37e0
Wala Motamed <motamedwala2@gmail.com>
2020-05-03 22:40:34 +0100

    Updates

---
+++ precache-manifest.b4b78e01315d525c4bf7109d3ec3952a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f7e729135c6033baafb61ab38a9e29a",
+    "url": "/Todo/index.html"
+  },
+  {
+    "revision": "d73722d684a4730341ea",
+    "url": "/Todo/static/css/2.5a8203ac.chunk.css"
+  },
+  {
+    "revision": "362e529970fdbc745a1e",
+    "url": "/Todo/static/css/main.7be1948a.chunk.css"
+  },
+  {
+    "revision": "d73722d684a4730341ea",
+    "url": "/Todo/static/js/2.e78bbe40.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Todo/static/js/2.e78bbe40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "362e529970fdbc745a1e",
+    "url": "/Todo/static/js/main.95a2fa65.chunk.js"
+  },
+  {
+    "revision": "637258f22119b41e0461",
+    "url": "/Todo/static/js/runtime-main.6605d98d.js"
+  }
+]);
